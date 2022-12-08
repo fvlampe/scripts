742e2fa14a62abb2dbf9ad3ed8d1ca410b15b3db
Friedemann von Lampe <fvonlampe@uni-goettingen.de>
2022-12-08 09:50:42 +0100

    Add files via upload

---
+++ plot.t.power.r
@@ -0,0 +1,81 @@
+# Funktion für Nicht-Zentralitätsparameter delta
+# d = Effektstärke, n = Stichprobengröße in Gruppe 1 & 2
+delta <- function(d, n1, n2) {
+  d * sqrt((n1*n2)/(n1+n2))
+}
+
+# Funktion für Verteilungskurven H0 und H1
+# in Abhängigkeit zum Nicht-Zentralitätsparameter
+# d = Effektstärke, n = Stichprobengröße in Gruppe 1 & 2
+plot.t.power <- function(d, n1, n2) {
+  
+  colalpha <- rgb(0, 0, 0, max = 255, alpha = 125, names = "gray50")
+  colbeta <- rgb(255, 0, 0, max = 255, alpha = 125, names = "red50")
+  
+  ncp <- delta(d, n1, n2)
+  N <- n1 + n2
+  
+  if(d >= 0) {
+    lo <- -6 
+    up <- 6 + ncp*2         # 1/ncp*15
+  } else {
+    lo <- -6 + ncp*2
+    up <- 6
+  }
+  
+  xt0 <-seq(lo, up, 0.01)
+  ydt0 <-dt(xt0, df = N-2)
+  plot(xt0, ydt0, type = "l",
+       xlab = "Value",
+       ylab = "Probability",
+       main = paste("t-Test power with N = ", N, "and d =", d))
+  
+  xt1 <-seq(lo, up, 0.01)
+  ydt1 <-dt(xt1, df = N-2, ncp = ncp)
+  lines(xt1, ydt1, type = "l", col = "red")
+  
+  # Area above critical value H0
+  locritvalue <-  qt(0.025, df = N-2)
+  upcritvalue <-  qt(0.975, df = N-2)
+  
+  # Upper alpha
+  polygon(c(xt0[xt0 >= upcritvalue ], upcritvalue),
+          c(ydt0[xt0 >= upcritvalue ], 0),
+          col = colalpha,
+          border = NA)
+  
+  # Lower alpha
+  polygon(c(xt0[xt0 <= locritvalue ], locritvalue),
+          c(ydt0[xt0 <= locritvalue ], 0),
+          col = colalpha,
+          border = NA)
+  
+  # Beta
+  if(d >= 0) {
+    polygon(c(xt1[xt1 <= upcritvalue ], upcritvalue),
+            c(ydt1[xt1 <= upcritvalue ], 0),
+            col = colbeta,
+            border = NA)
+  }
+  else {
+    polygon(c(xt1[xt1 >= locritvalue ], locritvalue),
+            c(ydt1[xt1 >= locritvalue ], 0),
+            col = colbeta,
+            border = NA)
+  }
+  
+  abline(v = c(qt(c(0.025, 0.975), df = N-2)),
+         lty = 2)
+  
+  legend("topright", 
+         c(expression(paste(alpha," (Type I error)")), 
+           expression(paste(beta," (Type II error)"))), 
+         fill = c(colalpha, colbeta),
+         bty = "n")
+  
+  legend("topleft", 
+         c("H0", "Critical values", "H1"), 
+         lty = c(1, 2, 1),
+         col = c("black", "black", "red"),
+         bty = "n")
+}
